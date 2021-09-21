0c2d8aa884a27f346e3bfdd22797b4d0735e6570
Sijia Liu <ymnliu@hotmail.com>
2021-09-21 09:36:20 -0500

    Initial commit

---
+++ server/src/main/java/org/openapitools/api/TextDateAnnotationsApiController.java
@@ -0,0 +1,24 @@
+package org.openapitools.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.NativeWebRequest;
+import java.util.Optional;
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2021-02-25T15:53:35.186246-08:00[America/Los_Angeles]")
+@Controller
+@RequestMapping("${openapi.nLPSandboxDateAnnotator.base-path:/api/v1}")
+public class TextDateAnnotationsApiController implements TextDateAnnotationsApi {
+
+    private final NativeWebRequest request;
+
+    @org.springframework.beans.factory.annotation.Autowired
+    public TextDateAnnotationsApiController(NativeWebRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return Optional.ofNullable(request);
+    }
+
+}
